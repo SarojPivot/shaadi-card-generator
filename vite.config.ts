bfb5a4a675d50f4882f3498343313b42b6202bf3
SarojPivot <mamamamialipur1@gmail.com>
2025-07-30 00:27:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shaadi-card-generator/',
+  base: '/',
     build: {
     rollupOptions: {
       input: {
