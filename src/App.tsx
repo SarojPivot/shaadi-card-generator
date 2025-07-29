6755e773923710e12362db0ab437ec348acce471
SarojPivot <mamamamialipur1@gmail.com>
2025-07-30 00:54:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Stack } from '@mui/material';
 import ShaadiCardGenerator from './pages/shaadi-card-generator';
 import Template1 from './pages/template-1/template-1';
 
 function App() {
   return (
-    <BrowserRouter>
       <Stack minHeight="100vh" sx={{ background: '#f8f4ff' }}>
         <Routes>
           <Route path="/" element={<ShaadiCardGenerator />} />
           <Route path="/download" element={<Template1 />} />
         </Routes>
       </Stack>
-    </BrowserRouter>
   );
 }
 
